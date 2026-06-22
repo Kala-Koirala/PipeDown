c7264a1edf6172577f5b8c69618ce2945c0f84e0
Shahisha <123805474+Shahisha1@users.noreply.github.com>
2026-06-22 20:07:45 +0545

    Update FlappyBird.java in the web

--- FlappyBird.java
+++ FlappyBird.java
@@ -14,8 +14,8 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     Image topPipeImg;
     Image bottomPipeImg;
 
-    int birdX = boardWidth/8;
-    int birdY = boardHeight/2;
+    int birdX = boardWidth / 8;
+    int birdY = boardHeight / 2;
     int birdWidth = 34;
     int birdHeight = 24;
 
@@ -55,9 +55,12 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     int gravity = 1;
 
     ArrayList<Pipe> pipes;
+    Random random = new Random();
 
     Timer gameLoop;
     Timer placePipesTimer;
+    boolean gameOver = false;
+    double score = 0;
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -78,14 +81,24 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
                 placePipe();
             }
         });
+        placePipesTimer.start();
 
-        gameLoop = new Timer(1000/60, this);
+        gameLoop = new Timer(1000 / 60, this);
         gameLoop.start();
     }
 
     public void placePipe() {
+
+        int randomPipeY = (int) (pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2));
+        int openingSpace = boardHeight / 4;
+
         Pipe topPipe = new Pipe(topPipeImg);
+        topPipe.y = randomPipeY;
         pipes.add(topPipe);
+
+        Pipe bottomPipe = new Pipe(bottomPipeImg);
+        bottomPipe.y = topPipe.y + pipeHeight + openingSpace;
+        pipes.add(bottomPipe);
     }
 
     public void paintComponent(Graphics g) {
@@ -94,35 +107,91 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     }
 
     private void draw(Graphics g) {
+
         g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+
         g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
-    }
+
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
+
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("Arial", Font.PLAIN, 24));
+            if(gameOver){
+                g.drawString("Game Over: "+ String.valueOf((int)score), 10, 35);
+
+            }else
+                {
+                    g.drawString("Score: "+ String.valueOf((int)score), 10, 35);
+                }
+            }
+
+        }
 
     public void move() {
         velocityY += gravity;
         bird.y += velocityY;
-        bird.y = Math.max(bird.y, 0); // ← fixed: was "+=" which was wrong
-    }
+        bird.y = Math.max(bird.y, 0);
+
+        // pipes
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            pipe.x += velocityX;
+
+            if (!pipe.passed && bird.x >pipe.x + pipe.width) {
+                pipe.passed = true;
+                score += 0.5;
+                
+            }
+            
+            if (collision(bird, pipe)) {
+                gameOver = true;
+            }
+
+            if (bird.y >boardHeight){
+                gameOver = true;
+            }}
+        }
+        public boolean collision(Bird a, Pipe b){
+            return a.x<b.x +b.width && 
+            a.x +a.width >b.x &&
+            a.y <b.y +b.height &&
+            a.y +a.height >b.y;
+        }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         move();
         repaint();
+        if(gameOver){
+            gameLoop.stop();
+            placePipesTimer.stop();
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             velocityY = -9;
+            if(gameOver){
+                //restart the game -- resetting all the conditions and variables
+                bird.y = birdY;
+                pipes.clear();
+                score = 0;
+                gameOver = false;
+                gameLoop.start();
+                placePipesTimer.start();
+            }
         }
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-    }                              // ← fixed: was missing closing brace
+    }
 
     @Override
     public void keyReleased(KeyEvent e) {
     }
 
-} 
+}
